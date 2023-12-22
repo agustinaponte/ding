a4fd83386e9fb12c77364d15a9e2dec9837ce2c5
agustinaponte <agustin.aponte@gmail.com>
2023-12-22 12:53:09 -0300

    First "working" version

--- README.md
+++ README.md
@@ -1,22 +1,42 @@
 # ding
  Like ping, but with sound
 
+____________.###+..+###-__________________________________________________________
+_____________+###..###+___________________________________________________________
+_________.+######..#######._________________█████__███_________________________
+________+#######.__.########_________________░░███__░░░_______________________________
+_______#####.__________.#####___________███████__████__████████____███████
+_______###._______________+##_________███_░░__███___░░███____░░███__░███__███__░░███
+______+##.________________.##+________░███_░__███__░_███__░__███__░███_░_███_░_███
+______###__________________###________░███_░__███__░_███____░███__░███__░███_░_███
+______##.__________________.##.________░░████████_█████__████_████__███████
+_____.##.__________________.##.__________________________░░░░░░░░_░░░░░_░░░░_░░░░░__░░░░░________________███
+_____###____________________###__________________________________________███_░███
+_____##+____________________+##___________________________________░░________██████
+____.##.____________________.##.___________________________________░░░░░░_______________
+____###______________________###____________Like_ping,_but_with_better_sound______
+___.############################._________________________________________________
++##################################+______________________________________________
+##.__________##########___________##______________________________________________
++###############.__.+##############+______________________________________________
+_____________##########___________________________________________________________
+________________####______________________________________________________________
+
+
 
 # What does ding do
-By default, ding pings a host and plays a sound each time it receives a response.
-
-
+By default, ding pings a host and plays a sound each time it receives a response and shows a small chart with latency.
 
 # Arguments
 
 `-h | --help`
 Shows help and exits
 
-`-c <thisMany> | --count <thisMany`
-Sets the amount of ping requests to `<thisMany>` and exits when done. By default, ding pings indefinetely
+`-c <thisMany> | --count <thisMany>
+Sets the amount of ping requests to `<thisMany>` and exits when done. By default, ding pings indefinetely.
 
 ` -l | --lost`
-Plays a sound only if some responses are not received
+Only plays a sound only if some responses are not received. Helpful to get notified when a host is not reachable.
 
 `-v | --verbose`
 Prints additional information
@@ -25,13 +45,13 @@ Prints additional information
 Checks for operating system and admin/root privileges
 
 If admin/root:
-	Uses lower level ping methods
+	Uses lower level ping methods (not implemented yet)
 Else:
-	Uses each os's ping binary
-
-
+	Uses each os's ping binary (Linux not implemented yet)
 
 # ToDo
+
+Use lower level ping methods if admin/root
 
 Packet size: Accept an argument to specify packet size
 
--- ding.py
+++ ding.py
@@ -1,86 +1,165 @@
 # -*- coding: utf-8 -*-
-"""
+"""                                                            
 Created on Mon Dec 18 22:46:04 2023
 @author: Agustin Aponte
-
-------------------------------------------------------------------------------------
-
-Arguments:
-    
-    -h | --help
-        Shows help and exits
-    
-    -c <thisMany> | --count <thisMany
-        Sets the amount of ping requests to <thisMany> and exits when done. By default, ding pings indefinetely
-        
-    -l | --lost
-        Plays a sound only if some responses are not received
-        
-    -v | --verbose
-        Prints additional information to standard output
-        
-------------------------------------------------------------------------------------
-
-Examples:
-    
-ding | Plays sound and shows help
-ding <host> | Pings a host and plays sound for every response
-ding -l <host> | Pings a host and plays a sound when some responses do not arrive
-ding -v <host> | Pings host indefinitely playing a sound for every response, Verbose mode
-ding -h | Shows help and exits
-ding --help | Shows help and exits
-ding -c 5 | Sends five ping requests and plays a sound for every response
-
-
 """
 import platform
 import os
 import subprocess
 import sys
-import getopt
+import logging
+import time
+import argparse
+
+
+ding_banner = """
+____________.###+..+###-_______________________________________________________
+_____________+###..###+________________________________________________________
+_________.+######..#######._________________█████__███_________________________
+________+#######.__.########_______________░░███__░░░__________________________
+_______#####.__________.#####____________███████__████__████████____███████____
+_______###._______________+##___________███░░███_░░███_░░███░░███__███░░███____
+______+##.________________.##+_________░███_░███__░███__░███_░███_░███_░███____
+______###__________________###_________░███_░███__░███__░███_░███_░███_░███____
+______##.__________________.##.________░░████████_█████_████_█████░░███████____
+_____.##.__________________.##._________░░░░░░░░_░░░░░_░░░░_░░░░░__░░░░░███____
+_____###____________________###____________________________________███_░███____
+_____##+____________________+##___________________________________░░██████_____
+____.##.____________________.##.___________________________________░░░░░░______
+____###______________________###____________Like_ping,_but_with_better_sound___
+___.############################.______________________________________________
++##################################+___________________________________________
+##.__________##########___________##___________________________________________
++###############.__.+##############+___________________________________________
+_____________##########________________________________________________________
+________________####___________________________________________________________
+"""
+operatingSystem = platform.system().lower()
+count = 0
+
+class Ping_result:
+    def __init__(self, result, latency):
+        # set self.result to True if a response was received, False if no response
+        self.result = result
+        self.latency = latency
+
+def printReadme():
+    # Prints README.md in running folder
+    with open('./README.md', 'r') as f:
+        print(f.read())
+    f.close()
 
 def parseArgs():
-    # Returns arguments
-    
-    # Default values
-    arg_help = False
-    arg_count = 0
-    arg_lost = False
-    arg_verbose = False
-    arg_host = 'localhost'
-    
     # Parse arguments
-    try: 
+    parser = argparse.ArgumentParser(
+        prog="ding",
+        description=ding_banner)
+    parser.add_argument('host',nargs='?')#, metavar='<host>', type=str, help=('Host to be pinged'
+    args = parser.parse_args(sys.argv[1:])
+    return args
+    #pts, args = getopt.getopt(argv[1:], "hc:lv", ["help", "count=", "lost", "verbose"])
+    #Return arg_help, arg_count, arg_packetLoss, arg_verbose, arg_host
     
-    
-    return arg_help, arg_count, arg_lost, arg_verbose, arg_host
-    
-def amAdmin():
+def runningAsAdmin():
     # Checks for root/admin privileges
-    if os.lower()=='windows': return 
-    if os.lower()=='linux': return 
-    if os.lower() != ('windows' or 'linux'): print("Could not recognize operating system")
-        
+    if operatingSystem=='windows': return 
+    if operatingSystem=='linux': return 
+    if operatingSystem != ('windows' or 'linux'): print("Could not recognize operating system")
     
-def play():
+def playSound():
+    # Play sound using motherboard speaker
     print('\a\b', end='')
     
-def ping(host):
-    param = '-n' if platform.system().lower()=='windows' else '-c'
-    command = ['ping', param, '1', host]
-    return subprocess.call(command) == 0
+def ping(host='localhost'):
+    # Pings once, returns:
+    #  0 if host responds
+    #  1 if host does not respond
+    #  2 if host was not found
+    def windowsPing(host):
+        def findResponseTime(ping_command_result):
+            for subtext in ping_command_result.split(" "):
+                if 'ms' in subtext:
+                    time = "".join(char for char in subtext if char.isdecimal())
+                    return time
+        param = '-n'
+        command = ['ping', param, '1', host]
+        text_result = subprocess.run(command, capture_output = True, text = True)
+        ping_command_result = text_result.stdout
+        errors = text_result.stderr
+        if "(0%" in ping_command_result: result = 0
+        if "(100%" in ping_command_result: result = 1
+        if "host" in ping_command_result: result = 2
+        latency = findResponseTime(ping_command_result)
+        return Ping_result(result,latency)
+    def linuxPing(host):
+        param = '-c'
+        command = ['ping', param, '1', host]
+        return subprocess.call(command,stdout=subprocess.PIPE) == 0
+    #print(host)
+    if operatingSystem=='windows': return windowsPing(host)
+    if operatingSystem =='linux': return linuxPing(host)
+    print("There is no ping command defined for this operating system ","(",operatingSystem,")")
 
+def printStatus(host,sent,received):
+    if operatingSystem=='windows':
+        os.system('cls')
+        #sys.stdout.write("\033[K")
+    percentage_received = (100*received/(sent if sent>0 else 1))
+    print("\r","Pinging",host,": Received/sent ",received,"/",sent,'(',str(int(percentage_received)),'% )')
+
+def printLatencyChart(resultv):
+    results_to_plot = 5
+
+    if len(resultv)<results_to_plot:
+        for resultn,result in enumerate(resultv):
+            if result[0]!=0:
+                print("No response")
+                break
+            print(result[1],"ms",end="")
+            print(" "*(6-len(str(result[1]))),end="")
+            print("▇"*int((int(result[1])+10)/20),end="")
+            print()
+    else:
+        resultv = resultv[-results_to_plot:]
+        for resultn,result in enumerate(resultv):
+            if result[0]!=0:
+                print("No response")
+                break
+            print(result[1],"ms",end="")
+            print(" "*(6-len(str(result[1]))),end="")
+            print("▇"*int((int(result[1])+10)/20),end="")
+            print()
+        
+
+    
 #
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
 
-# Get OS information
-operatingSystem = platform.system()
-
-# Parse command line arguments:
-parseArgs()
+def ding():
+    argv=sys.argv
+    cont=True
+    sent=0
+    received=0
+    resultv=[]
     
+    # Parse command line arguments:
+    argv = parseArgs()
+    while cont == True:
+        response = ping(argv.host)
+        sent+=1
+        print(response.result)
+        if response.result==0:
+            playSound()
+            received+=1
+        if response.result==2:
+            print("Host",argv.host,"not found")
 
-
-
-
+        resultv.append([response.result, response.latency])
+        printStatus(argv.host,sent,received)
+        printLatencyChart(resultv)
+        time.sleep(2)
+        
+    
+if __name__ == "__main__":
+    ding()
